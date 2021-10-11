f3f5a5df225badc0433f560bd2ab0e083a7c89d6
ComAz007 <maxspamm@mail.ru>
2021-10-11 13:24:14 +0300

    123

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.firstapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private val inputText:EditText by lazy { findViewById(R.id.inputText) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.sendMessage)
+        button.setOnClickListener {
+            val text = "Пора покормить кота!"
+            val duration = Toast.LENGTH_LONG
+
+            val toast = Toast.makeText(applicationContext, text, duration)
+            //toast.setGravity(Gravity.CENTER, 0, 0)
+            toast.show()
+
+            val textView: TextView = findViewById(R.id.getText);
+            textView.text =inputText.text;
+        }
+        //OnClickSendMessage(view: View);
+//        val textView: TextView = findViewById(R.id.GetText) as TextView
+//        TextView.setOnClickListener {
+//            TextView.text = getString(R.id.InputText)
+
+    }
+
+        fun randomMe(view: View) {
+            val randomIntent = Intent(this, MainActivity2::class.java)
+            randomIntent.putExtra(MainActivity2.TOTAL_COUNT, 15)
+            startActivity(randomIntent)
+        }
+
+//    fun OnClickSendMessage(view: View) {
+//        val textView: TextView = findViewById(R.id.getText);
+//       // val inputText:EditText = findViewById(R.id.inputText);
+//        val toast =Toast.makeText(this, "mmmtext",Toast.LENGTH_LONG);
+//        toast.setGravity(Gravity.CENTER, 50, 50)
+//        //textView.text =inputText.getText();
+//    }
+
+    fun OnСlickCount(view: View) {
+        val textView: TextView = findViewById(R.id.getText);
+        val countString = textView.text.toString()
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+        // Display the new value in the text view.
+        textView.text = count.toString();
+    }
+}
---
+++ app/src/main/java/com/example/firstapp/MainActivity2.kt
@@ -0,0 +1,43 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import java.util.*
+
+class MainActivity2 : AppCompatActivity() {
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        showRandomNumber()
+    }
+
+    fun showRandomNumber() {
+        // Get the count from the intent extras
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+
+        // Generate the random number
+        val random = Random()
+        var randomInt = 0
+
+        // Add one because the bound is exclusive
+        if (count > 0) {
+            // Add one because the bound is exclusive
+            randomInt = random.nextInt(count + 1)
+        }
+
+        // Display the random number.
+        val textview_random: TextView = findViewById(R.id.textview_random);
+        val textview_label: TextView = findViewById(R.id.textview_label);
+        textview_random.text = Integer.toString(randomInt)
+
+        // Substitute the max value into the string resource
+        // for the heading, and update the heading
+        textview_label.text = getString(R.string.random_heading, count)
+
+    }
+}
